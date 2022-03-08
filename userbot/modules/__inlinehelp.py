26bb1d353c9a5e8e3f46a89341677d127e670234
Fariz <84852749+farizjs@users.noreply.github.com>
2022-03-08 10:33:20 +0700

    Update __inlinehelp.py

--- userbot/modules/__inlinehelp.py
+++ userbot/modules/__inlinehelp.py
@@ -1,32 +1,84 @@
-import logging
+#    TeleBot - UserBot
+#    Copyright (C) 2020 TeleBot
 
-from userbot import BOT_USERNAME, BOT_TOKEN
-from telethon.errors.rpcerrorlist import BotInlineDisabledError
-from userbot.utils import flicks_cmd
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
-    level=logging.WARNING)
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+
+from telebot import ALIVE_NAME, BOT_USERNAME, CMD_HELP, CMD_HANDLER, CMD_LIST, bot, tgbot
+
+user = bot.get_me()
+DEFAULTUSER = user.first_name
+CUSTOM_HELP_EMOJI = "⚡"
 
 
-@flicks_cmd(pattern="helpme")
-async def yardim(event):
-    flicksbotusername = BOT_USERNAME
-    if flicksbotusername and BOT_TOKEN:
-        try:
-            results = await event.client.inline_query(
-                flicksbotusername, "@FlicksSupport"
-            )
-        except BotInlineDisabledError:
-            return await event.edit(
-                "`Bot tidak dapat digunakan dalam mode sebaris\nPastikan untuk mengaktifkan mode sebaris!`"
-            )
-        await results[0].click(
-            event.chat_id, reply_to=event.reply_to_msg_id, hide_via=False
-        )
-        await event.delete()
-    else:
-        return await event.edit(
-            "`Botnya tidak berfungsi! Silakan atur Bot Token dan Nama Pengguna dengan benar`"
-            "\n`Plugin telah dihentikan`"
-        )
+
+
+@flicks_cmd(pattern="help ?(.*)")
+async def cmd_list(event):
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        tgbotusername = BOT_USERNAME
+        input_str = event.pattern_match.group(1)
+        if tgbotusername is None or input_str == "text":
+            string = ""
+            for i in CMD_HELP:
+                string += CUSTOM_HELP_EMOJI + " " + i + " " + CUSTOM_HELP_EMOJI + "\n"
+                for iter_list in CMD_HELP[i]:
+                    string += "    `" + str(iter_list) + "`"
+                    string += "\n"
+                string += "\n"
+            if len(string) > 4095:
+                with io.BytesIO(str.encode(string)) as out_file:
+                    out_file.name = "cmd.txt"
+                    await tgbot.send_file(
+                        event.chat_id,
+                        out_file,
+                        force_document=True,
+                        allow_cache=False,
+                        caption="**COMMANDS**",
+                        reply_to=reply_to_id,
+                    )
+                    await event.delete()
+            else:
+                await event.edit(string)
+        elif input_str:
+            if input_str in CMD_LIST:
+                string = "**Commands available in {}** \n\n".format(input_str)
+                if input_str in CMD_HELP:
+                    for i in CMD_HELP[input_str]:
+                        string += i
+                    string += "\n\n**© @TheFlicksUserbot**"
+                    await event.edit(string)
+                else:
+                    for i in CMD_LIST[input_str]:
+                        string += "    " + i
+                        string += "\n"
+                    string += "\n**© @TheFlicksUserbot**"
+                    await event.edit(string)
+            else:
+                await event.edit(input_str + " bukan plugin yang valid!")
+        else:
+            help_string = f"""Pembantu Userbot untuk {DEFAULTUSER}"""
+            try:
+                results = await bot.inline_query(  # pylint:disable=E0602
+                    tgbotusername, help_string
+                )
+                await results[0].click(
+                    event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+                )
+                await event.delete()
+            except BaseException:
+                await event.edit(
+                    f"Bot ini telah dinonaktifkan sebaris. Harap aktifkan untuk digunakan `{CMD_HANDLER}help`"
+                )

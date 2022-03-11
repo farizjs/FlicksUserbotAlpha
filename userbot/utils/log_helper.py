4a2c5f19d198897f228664857f7f53369cf73f15
Fariz <84852749+farizjs@users.noreply.github.com>
2022-03-12 06:44:04 +0700

    Create log_helper.py

---
+++ userbot/utils/log_helper.py
@@ -0,0 +1,44 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2022 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
+
+# Recode by @farizjs
+# FROM Flicks-Userbot <https://github.com/farizjs/FlicksUserbotAlpha>
+# t.me/TheFlicksUserbot
+
+from userbot.modules.sql_helper.globals import gvarstatus, addgvar
+
+
+def get_stuff():
+    return sql.gvarstatus("BOTCHAT") or {}
+
+
+def add_stuff(msg_id, user_id):
+    ok = get_stuff()
+    ok.update({msg_id: user_id})
+    return sql.addgvar("BOTCHAT", ok)
+
+
+def get_who(msg_id):
+    ok = get_stuff()
+    if ok.get(msg_id):
+        return ok[msg_id]
+
+
+def tag_add(msg, chat, user):
+    ok = get_stuff()
+    if not ok.get("TAG"):
+        ok.update({"TAG": {msg: [chat, user]}})
+    else:
+        ok["TAG"].update({msg: [chat, user]})
+    return sql.gvarstatus("BOTCHAT", ok)
+
+
+def who_tag(msg):
+    ok = get_stuff()
+    if ok.get("TAG") and ok["TAG"].get(msg):
+        return ok["TAG"][msg]
+    return False, False
